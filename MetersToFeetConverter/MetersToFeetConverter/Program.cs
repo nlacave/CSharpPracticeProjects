5a82162a55b3f18131808167d002bc5c64028b7f
nlacave <lacavenicolasleonel@gmail.com>
2025-02-20 16:51:45 -0300

    Initial commit

---
+++ MetersToFeetConverter/MetersToFeetConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+while (true)
+{
+    Console.WriteLine("Unit Converter - Meters to Feet");
+    Console.WriteLine("Enter the number of meters to convert:");
+    string meters = Console.ReadLine();
+
+    // Reemplaza la coma con punto para asegurar que el valor sea interpretado correctamente
+    meters = meters.Replace(',', '.');
+
+    // Intenta convertir el valor utilizando InvariantCulture para manejar el punto como separador decimal
+    if (!double.TryParse(meters, NumberStyles.Any, CultureInfo.InvariantCulture, out double metersToDouble) || metersToDouble <= 0)
+    {
+        Console.WriteLine("The data entered is not a correct value");
+    }
+    else
+    {
+        double feetDecimal = Math.Round(metersToDouble * 3.28, 2);
+        Console.WriteLine($"The number of meters to feet is: {feetDecimal}");
+    }
+
+    Console.WriteLine("Do you want to convert a new value of meters? Press any key... or letter E for Exit");
+    var keyInfo = Console.ReadKey();
+    if (keyInfo.Key == ConsoleKey.E)
+    {
+        break;
+    }
+    else
+    {
+        Console.Clear();
+    }
+}
---
+++ SimplyCalculator/SimplyCalculator/Program.cs
@@ -0,0 +1,71 @@
+﻿while (true)
+{
+    Console.WriteLine("Simply Calculator");
+    Console.WriteLine("Press any key for initiate the operation or letter E for Exit.");
+    var letter = Console.ReadKey();
+    if (letter.Key == ConsoleKey.E)
+    {
+        return;
+    }
+    Console.WriteLine("\nEnter the first number to calculate:");
+    string firstValue = Console.ReadLine();
+    double firstNumber = 0;
+    bool allElementsValid = true;
+    if (double.TryParse(firstValue, out double value))
+    {
+        firstNumber = value;
+    }
+    else
+    {
+        allElementsValid = false;
+    }
+
+    Console.WriteLine("Enter the second number to calculate:");
+    string secondValue = Console.ReadLine();
+    double secondNumber = 0;
+    if (double.TryParse(secondValue, out double value2))
+    {
+        secondNumber = value2;
+    }
+    else
+    {
+        allElementsValid = false;
+    }
+
+    Console.WriteLine("Enter the operation sign: + - * /");
+    string operationSign = Console.ReadLine();
+    double operationValue = 0;
+    if (operationSign == "+")
+    {
+        operationValue = firstNumber + secondNumber;
+    }
+    else if (operationSign == "-")
+    {
+        operationValue = firstNumber - secondNumber;
+    }
+    else if (operationSign == "*")
+    {
+        operationValue = firstNumber * secondNumber;
+    }
+    else if (operationSign == "/")
+    {
+        operationValue = firstNumber / secondNumber;
+    }
+    else
+    {
+        allElementsValid = false;
+    }
+
+    if (!allElementsValid || secondNumber == 0 && operationSign == "/")
+    {
+        Console.WriteLine("\nThe operation is invalid. Try to enter the values again.");
+    }
+    else
+    {
+        Console.WriteLine($"{firstNumber} {operationSign} {secondNumber} = {operationValue}");
+    }
+
+    Console.WriteLine("\nPress any key to clean the console.");
+    Console.ReadKey();
+    Console.Clear();
+}
